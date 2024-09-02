057883dc92a61e5f1ae149eebcf9019c5c43d2cb
Ahmet E. Dag <adag21044@gmail.com>
2024-09-02 17:12:09 +0300

    Placement issue

--- Assets/Scripts/ItemGrid.cs
+++ Assets/Scripts/ItemGrid.cs
@@ -7,13 +7,31 @@ public class ItemGrid : MonoBehaviour
     const float tileSizeWidth = 32f;
     const float tileSizeHeight = 32f;
 
+    InventoryItem[,] inventoryItemSlot;
+
     RectTransform rectTransform;
+
+    [SerializeField] int gridSizeWidth = 20;
+    [SerializeField] int gridSizeHeight = 10;
+
+    [SerializeField] GameObject inventoryItemPrefab;
+
     Vector2 positionOnTheGrid = new Vector2();
     Vector2Int tileGridPosition = new Vector2Int();
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        Init(gridSizeWidth, gridSizeHeight);
+        InventoryItem inventoryItem = Instantiate(inventoryItemPrefab).GetComponent<InventoryItem>();
+        PlaceItem(inventoryItem, 3, 2);
+    }
+
+    private void Init(int width, int height)
+    {
+        inventoryItemSlot = new InventoryItem[width, height];
+        Vector2 size = new Vector2(width * tileSizeWidth, height * tileSizeHeight);
+        rectTransform.sizeDelta = size;
     }
 
     public Vector2Int GetTileGridPosition(Vector2 mousePosition)
@@ -25,5 +43,20 @@ public class ItemGrid : MonoBehaviour
         tileGridPosition.y = (int)(positionOnTheGrid.y / tileSizeHeight);
 
         return tileGridPosition;
+    }
+
+    public void PlaceItem(InventoryItem inventoryItem, int posX, int posY)
+    {
+        RectTransform rectTransform = inventoryItem.GetComponent<RectTransform>();
+
+        rectTransform.SetParent(this.rectTransform);
+
+        inventoryItemSlot[posX, posY] = inventoryItem;
+
+        Vector2 position = new Vector2();
+        position.x = posX * gridSizeHeight + gridSizeWidth / 2;
+        position.y = -(posY * gridSizeHeight + gridSizeHeight / 2);
+
+        rectTransform.localPosition = position;
     }
 }
